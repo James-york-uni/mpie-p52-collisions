573536564fea4edb49f3d8ccb1729de25d2d14b6
James-york-uni <zzw518@york.ac.uk>
2025-10-23 17:41:50 +0100

    gun

--- Assets/Collide.cs
+++ Assets/Collide.cs
@@ -7,15 +7,29 @@ public class Collide : MonoBehaviour
     {
         
     }
-    public float Ammo = 0;
+    public float ammo = 0;
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray =  Camera.main.ViewportPointToRay(new Vector3(0.5f ,0.5f ,0.0f));
+        if(Input.GetKeyDown (KeyCode.Mouse0)) {
+            if(ammo > 0) {
+                ammo --; 
+            RaycastHit result; 
+            Physics.Raycast(ray, out result);
+            GameObject g = result.collider.gameObject;
+            if(result.collider.gameObject.name == "Target") {
+                Animation a = g.transform.parent.GetComponent<Animation>();
+                a.Play("LowerBridge");
+            }
+        }
+        }
     }
 
     void OnTriggerEnter(Collider other) {
-        Ammo += 20;
-        GameObject.SetActive(false);   
+        if(other.gameObject.name == "AmmoBox") {
+            ammo += 20;
+            other.gameObject.SetActive(false);   
+        }
     }
 }
