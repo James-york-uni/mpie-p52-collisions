4b8e65bab369b5a973f557d8e6c4eceb0c7f1e7e
James-york-uni <zzw518@york.ac.uk>
2025-11-12 10:54:55 +0000

    audio

--- Assets/Collide.cs
+++ Assets/Collide.cs
@@ -13,6 +13,10 @@ public class Collide : MonoBehaviour
     {
         Ray ray =  Camera.main.ViewportPointToRay(new Vector3(0.5f ,0.5f ,0.0f));
         if(Input.GetKeyDown (KeyCode.Mouse0)) {
+            if(ammo == 0) {
+               AudioSource gunEmpty = GetComponent<AudioSource>(); 
+               gunEmpty.Play();
+            }
             if(ammo > 0) {
                 ammo --; 
             RaycastHit result; 
@@ -28,6 +32,8 @@ public class Collide : MonoBehaviour
 
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.name == "AmmoBox") {
+            AudioSource gunEmpty = GetComponent<AudioSource>(); 
+            gunEmpty.Play();
             ammo += 20;
             other.gameObject.SetActive(false);   
         }
